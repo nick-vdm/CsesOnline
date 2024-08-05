b06aa570e725363cd806e282c74a5bcf8ca4a504
nick-vdm <nick.vdm.dev@gmail.com>
2024-08-05 11:50:36 +1000

    middle bar looks better

--- src/frontend/src/ProblemView/ProblemViewView.tsx
+++ src/frontend/src/ProblemView/ProblemViewView.tsx
@@ -49,7 +49,31 @@ const ProblemViewView: React.FC<ProblemViewViewProps> = ({ problem }) => {
           >
             <ProblemPage problem={problem} />
           </div>
-          <div className="divider" onMouseDown={handleMouseDown} />
+          <div
+            className="divider"
+            onMouseDown={handleMouseDown}
+            style={{
+              width: '10px',
+              backgroundColor: '#21252b',
+              cursor: 'col-resize',
+              position: 'relative',
+            }}
+          >
+            <div
+              style={{
+                width: '6px',
+                height: '20px',
+                backgroundColor: '#fff',
+                borderRadius: '3px',
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                transition: 'background-color 0.3s',
+              }}
+              className="divider-dent"
+            />
+          </div>
           <div
             className="pane right-pane"
             style={{ width: `${100 - dividerPosition}%` }}
